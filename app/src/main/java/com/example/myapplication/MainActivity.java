9f5f7e9c091307162501e213fef4a938a52297af
pupil <matches707@gail.com>
2020-01-22 15:45:49 +0200

    NewCats

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("AnotherOftest");
+        ArrayList<Cat> cats=new ArrayList<>();
+        print(cats);
+    }
+
+    private void print(ArrayList<Cat> cats){
+        for (int i=0;i<cats.size();i++){
+            System.out.println("Cats name :" + cats.get(i).getName());
+        }
     }
 }
